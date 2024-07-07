df85257254079c03da9492f8122ba2df46a9d56e
Douglas Fanucchi <douglasfanucchi@gmail.com>
2024-07-07 10:31:50 -0300

    tests: test changes raw bits

--- includes/Fixed.hpp
+++ includes/Fixed.hpp
@@ -3,9 +3,13 @@
 
 class Fixed
 {
+    private:
+        int _raw;
+
     public:
         Fixed();
         int getRawBits(void) const;
+        void setRawBits(int const raw);
 };
 
 #endif
--- tests/unit/Fixed.cpp
+++ tests/unit/Fixed.cpp
@@ -11,7 +11,19 @@ void test_should_create_a_fix_point()
     ASSERT_EQ(expected, result);
 }
 
+void test_should_change_raw_bits()
+{
+    Fixed fixed;
+    int const expected = 25;
+
+    fixed.setRawBits(25);
+    int result = fixed.getRawBits();
+
+    ASSERT_EQ(expected, result);
+}
+
 void RUN_EXAMPLE_SUITE()
 {
     test_should_create_a_fix_point();
+    test_should_change_raw_bits();
 }
