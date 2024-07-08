d90ff8dc5f97557e35e764e16f1b0a398c4526ba
Douglas Fanucchi <douglasfanucchi@gmail.com>
2024-07-08 12:56:10 -0300

    tests: test copies object using assignment copy operator

--- tests/unit/Fixed.cpp
+++ tests/unit/Fixed.cpp
@@ -33,9 +33,21 @@ void test_should_copy_using_copy_constructor()
     ASSERT_EQ(expected, result.getRawBits());
 }
 
+void test_should_copy_using_assignment_operator()
+{
+    Fixed fixed, result;
+    fixed.setRawBits(25);
+    int expected = 25;
+
+    result = fixed;
+
+    ASSERT_EQ(expected, result.getRawBits());
+}
+
 void RUN_EXAMPLE_SUITE()
 {
     test_should_create_a_fix_point();
     test_should_change_raw_bits();
     test_should_copy_using_copy_constructor();
+    test_should_copy_using_assignment_operator();
 }
